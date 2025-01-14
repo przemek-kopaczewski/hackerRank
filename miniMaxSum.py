1c4a28ace2ed58c8afdb6c6829cb12be9db66420
Przemysław Kopaczewski <kopaczewski.przemek@gmail.com>
2025-01-14 10:22:00 +0100

    Add README

--- miniMaxSum.py
+++
@@ -1,9 +0,0 @@
-def miniMaxSum(arr):
-    sumMin = sum(arr) - max(arr)
-    sumMax = sum(arr) - min(arr)
-
-    print(str(sumMin) + " " + str(sumMax))
-
-
-arr = list(map(int, input().rstrip().split()))
-miniMaxSum(arr)
--- timeConversion.py
+++
@@ -1,8 +0,0 @@
-from datetime import datetime
-def timeConversion(s):
-    time_object = datetime.strptime(s, '%I:%M:%S%p')
-    return time_object.strftime("%H:%M:%S")
-
-s = input()
-result = timeConversion(s)
-print(result)
